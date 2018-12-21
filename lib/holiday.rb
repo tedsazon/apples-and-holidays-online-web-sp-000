a89e7f1482e17aaabd6d38b848dbd91758ef0cd3
Ted Sazon <tedsazon@gmail.com>
2018-12-21 18:36:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -41,7 +41,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holidays|
     holidays.map do |holiday, supplies|
-      holiday if supplies.inlude?("BBQ")
+      holiday if supplies.include?("BBQ")
       end
     end.flatten.compact
   
